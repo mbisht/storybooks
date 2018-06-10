a29eb57b0e4c4fa372c1482884b36e8d72f65e42
mbisht <mayankbisht.com@gmail.com>
2018-06-10 16:41:29 +0530

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports =  {
+  mongoURI:'mongodb://mayankbisht:26061990poiuytrewq@ds135760.mlab.com:35760/storybooks-tev',
+  googleClientID:'792311470790-3hpv6vqspqeuvb886faaudfdiambeb5q.apps.googleusercontent.com',
+  googleClientSecret:'hlPpmsyQJiLQO8sPQB_ivWnA '
+}
