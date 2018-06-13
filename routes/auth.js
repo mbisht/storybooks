d342b6acd1999ebdc4144c38593dbc53a26d5281
mbisht <mayankbisht.com@gmail.com>
2018-06-13 21:40:36 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-  // res.send('auth');
 
-  router.get('/google/callback', 
-   passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
-    // Successful authentication, redirect home.
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
     res.redirect('/dashboard');
   });
+
+router.get('/verify', (req, res) => {
+  if(req.user){
+    console.log(req.user);
+  } else {
+    console.log('Not Auth');
+  }
+});
+
+router.get('/logout', (req, res) => {
+ req.logout();
+ res.redirect('/');
+});
 
 module.exports = router;
