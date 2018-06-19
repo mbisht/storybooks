b3896b8484f963c56e1007a2bca101eb26376934
mbisht <mayankbisht.com@gmail.com>
2018-06-19 21:03:47 +0530

    started UI

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -11,6 +12,7 @@ require('./models/User');
 require('./config/passport')(passport);
 
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
 // Load Keys
@@ -27,9 +29,11 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
-});
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(session({
@@ -49,6 +53,7 @@ app.use((req, res, next) => {
 });
 
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 
