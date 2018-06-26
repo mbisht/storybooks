ef5652ce006a847fef88b3e7f60743423f627a67
mbisht <mayankbisht.com@gmail.com>
2018-06-26 11:57:20 +0530

    version 1.0.0alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports =  {
-  mongoURI:'mongodb://mayankbisht:26061990poiuytrewq@ds135760.mlab.com:35760/storybooks-tev',
-  googleClientID:'792311470790-3hpv6vqspqeuvb886faaudfdiambeb5q.apps.googleusercontent.com',
-  googleClientSecret:'hlPpmsyQJiLQO8sPQB_ivWnA '
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
